c2286aaafe0298479725e5680b94ad3f1f9e2780
Rob Taylor <roboncode@gmail.com>
2022-01-26 20:58:36 -0700

    tailwind component updates

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,7 @@ const currentPage = ref(0)
           v-model="text"
           multiline
           class="border wi-border"
+          class-name="app-textinput"
         />
         <VButton size="xl" class="wi-primary w-full">Submit</VButton>
         <VSpinner class="animate-spin" />
@@ -42,11 +43,12 @@ const currentPage = ref(0)
         </div>
         <div class="flex justify-center items-center gap-8">
           <VSpinner
-            class="border-2 border-green-400 border-r-transparent border-t-transparent"
+            class="w-6 h-6 border-2 border-green-400 border-r-transparent border-t-transparent"
+            class-name="app-spinner"
           />
 
           <VSpinner
-            class="wi-fastest w-10 h-10 border-4 border-blue-500 border-r-transparent border-l-transparent"
+            class="wi-fastest w-8 h-8 border-4 border-blue-500 border-r-transparent border-l-transparent"
           />
 
           <VSpinner
@@ -77,12 +79,42 @@ const currentPage = ref(0)
               class="transition duration-150 transform"
               :class="{ 'rotate-180': isOpen }"
                 />-->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="transition duration-150 transform"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  :class="{ 'rotate-90': isOpen }"
                 >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <polyline points="9 6 15 12 9 18" />
+                </svg>
               </div>
             </template>
             <div class="border-b border-r border-l wi-border wi-light-dark p-4">
               <VSkeletonListItem />
               <VSkeletonListItem />
+              <!-- <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                <div class="animate-pulse flex space-x-4">
+                  <div class="rounded-full bg-slate-700 h-10 w-10"></div>
+                  <div class="flex-1 space-y-6 py-1">
+                    <div class="h-2 bg-slate-700 rounded"></div>
+                    <div class="space-y-3">
+                      <div class="grid grid-cols-3 gap-4">
+                        <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+                        <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+                      </div>
+                      <div class="h-2 bg-slate-700 rounded"></div>
+                    </div>
+                  </div>
+                </div>
+              </div> -->
             </div>
           </VCollapse>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,32 @@ module.exports = {
         },
         dark: colors.slate,
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: 'inherit',
+            a: {
+              'color': 'inherit',
+              'opacity': 0.75,
+              'fontWeight': '500',
+              'textDecoration': 'underline',
+              '&:hover': {
+                opacity: 1,
+                color: colors.teal[600],
+              },
+            },
+            b: { color: 'inherit' },
+            strong: { color: 'inherit' },
+            em: { color: 'inherit' },
+            h1: { color: 'inherit' },
+            h2: { color: 'inherit' },
+            h3: { color: 'inherit' },
+            h4: { color: 'inherit' },
+            code: { color: 'inherit' },
+          },
+        },
+      },
     },
   },
   plugins: [
