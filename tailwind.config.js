1e2430ed9168b54612b2f1f4c503d6d259cd7bd3
Rob Taylor <roboncode@gmail.com>
2022-01-27 05:59:21 -0700

    removed windicss deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
-// const defaultTheme = require('tailwindcss/defaultTheme')
-// const { safelist } = require('@vuwi/plugin/dist/tailwindcss')
 const colors = require('tailwindcss/colors')
-// const typography = require('@tailwindcss/typography')
-  // "exports": {
-  //   "./vuwi.css": "./dist/vuwi.css",
-  //   "./safelist": "./dist/safelist.js",
-  //   "./dist/tailwindcss": "./dist/tailwindcss.js",
-  //   "./tailwindcss": "./dist/tailwindcss.js",
-  //   "./dist/windicss": "./dist/windicss.js",
-  //   "./windicss": "./dist/windicss.js"
-  // },
-const safelist = require('@vuwi/plugin/safelist')
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html', 
+    './src/**/*.{vue,js,ts,jsx,tsx}', 
+    // './node_modules/@vuwi/vue/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   safelist: require('@vuwi/plugin/safelist'),
   theme: {
     minWidth: {
@@ -60,5 +52,6 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@vuwi/plugin/tailwindcss'),
+    // require('@vuwi/css/tailwind'),
   ],
 }
